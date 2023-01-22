8c5013a50b4169c721f16de2a42b33683348d41b
CoalStones <123333323+CoalStones@users.noreply.github.com>
2023-01-22 17:11:11 -0500

    Add files via upload

---
+++ Ch3Ex7.java
@@ -0,0 +1,21 @@
+
+package ch3ex7;
+
+
+public class Ch3Ex7 {
+
+    public static void main(String[] args) {
+        System.out.println("     *****");
+        System.out.println("   *       *");
+        System.out.println("  * _     _ *");
+        System.out.println(" *  o     o  *");
+        System.out.println("*      |      *");
+        System.out.println("*      |      *");
+        System.out.println(" *     +     *");
+        System.out.println("  * \\_____/ *");
+        System.out.println("   *       *");
+        System.out.println("     *****");
+        
+    }
+    
+}
